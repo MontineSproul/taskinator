2ab76b2818762a42b34e75d4cba871c5b1ecc6fc
Ashley Montine Sproul <montinesproul@gmail.com>
2023-02-23 23:31:21 -0500

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var buttonEl = document.querySelector('#save-task');
-console.log(buttonEl);
+var taskToDoEl = document.querySelector('#tasks-to-do');
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement('li');
+    listItemEl.className = 'task-items';
+    listItemEl.textContent = 'This is a new task.';
+    taskToDoEl.appendChild(listItemEl);
+}
+buttonEl.addEventListener('click', createTaskHandler);
 
