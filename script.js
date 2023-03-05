ffac0b20e65deefd560abd4f53e7ec4e6d20bb03
Ashley Montine Sproul <montinesproul@gmail.com>
2023-03-04 22:47:59 -0500

    add ability to submit custom tasks

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
- //package up data as an object
- var taskDataObj =  {
-    name: taskNameInput,
-    type: taskTypeInput
- };
- //send it as an argument to createTaskEL
- createTaskEL(taskDataObj);
+    //check if input values are empty strings
+    if(!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    }
+    formEl.reset();
+    //package up data as an object
+    var taskDataObj =  {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    //send it as an argument to createTaskEL
+    createTaskEL(taskDataObj);
 }
 
 var createTaskEL = function(taskDataObj) {
